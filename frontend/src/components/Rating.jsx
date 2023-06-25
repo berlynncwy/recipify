d0270125a51ae2119942d19da19a3732ddfa0979
berlynncwy <123068804+berlynncwy@users.noreply.github.com>
2023-06-25 17:19:21 +0800

    Create rating component

---
+++ frontend/src/components/Rating.jsx
@@ -0,0 +1,57 @@
+import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
+
+const Rating = ({ rating, noOfReviews }) => {
+  return (
+    <div className="rating flex">
+      <span>
+        {rating >= 1 ? (
+          <FaStar />
+        ) : rating >= 0.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {rating >= 2 ? (
+          <FaStar />
+        ) : rating >= 1.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {rating >= 3 ? (
+          <FaStar />
+        ) : rating >= 2.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {rating >= 4 ? (
+          <FaStar />
+        ) : rating >= 3.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {rating >= 5 ? (
+          <FaStar />
+        ) : rating >= 4.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+
+      <span className="rating-text">{noOfReviews && noOfReviews}</span>
+    </div>
+  );
+};
+
+export default Rating;
