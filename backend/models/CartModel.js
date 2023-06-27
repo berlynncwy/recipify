ea00014268485a5c1a494a3ba203856695194ef7
berlynncwy <123068804+berlynncwy@users.noreply.github.com>
2023-06-27 21:44:51 +0800

    Update CartModel

--- backend/models/CartModel.js
+++ backend/models/CartModel.js
@@ -6,21 +6,32 @@ const cartSchema = new mongoose.Schema({
         ref: "Customer",
         required: true
     },
-    items: [{
-        product: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Product",
-            required: true
-        },
-        quantity: {
-            type: Number,
-            required: true
-        }
-    }],
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+    items: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true
+            },
+            image: {
+                type: String,
+                required: true
+            },
+            price: {
+                type: Number,
+                required: true
+            },
+            products: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product",
+                required: true
+            },
+        }],
+}, {
+    timestamps: true
 });
 const Cart = mongoose.model("Cart", cartSchema);
 
