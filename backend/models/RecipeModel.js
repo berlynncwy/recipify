1f45307ad77b1eef174dd06cc34f33fd0361f228
berlynncwy <123068804+berlynncwy@users.noreply.github.com>
2023-06-27 16:15:23 +0800

    Update RecipeModel

--- backend/models/RecipeModel.js
+++ backend/models/RecipeModel.js
@@ -1,5 +1,31 @@
 import mongoose from "mongoose";
 
+const reviewSchema = new mongoose.Schema({
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment: {
+        type: String
+    },
+},
+    {
+        timestamps: true
+    }
+);
+
 const recipeSchema = new mongoose.Schema({
     author: {
         type: mongoose.Schema.Types.ObjectId,
@@ -15,7 +41,7 @@ const recipeSchema = new mongoose.Schema({
         required: true
     },
     ingredients: [{
-        type: String,
+        name: String,
         quantity: Number,
         unit: String,
         required: true
@@ -40,17 +66,27 @@ const recipeSchema = new mongoose.Schema({
         required: true
     },
     image: {
-        type: [String]
+        type: [String],
+        required: true
     },
-    createdAt: {
-        type: Date,
-        default: Date.now
+
+    review: [reviewSchema],
+    rating: {
+        type: Number,
+        required: true,
+        default: 0,
     },
-    updatedAt: {
-        type: Date,
-        default: Date.now
+    numReviews: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+},
+    {
+        timestamps: true
     }
-});
+);
+
 const Recipe = mongoose.model("Recipe", recipeSchema);
 
 export default Recipe;
