fbe450a6b36a82bccfe9e04c2b7c7688bb08e3a8
berlynncwy <123068804+berlynncwy@users.noreply.github.com>
2023-07-03 23:47:53 +0800

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+
+import App from "./App.jsx";
+import ErrorPage from "./pages/ErrorPage";
+import Cart from "./pages/Cart";
+import MyAccount from "./pages/MyAccount";
+import Product from "./components/Product";
+import HomePage from "./pages/HomePage";
+import RecipePage from "./pages/RecipePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomePage />} />
+      <Route path="/recipe/:id/" element={<RecipePage />} />
+      <Route path="/products" element={<Product />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/account" element={<MyAccount />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/*" element={<Navigate to="/error" replace />} />
+      <Route path="/error" element={<ErrorPage />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
