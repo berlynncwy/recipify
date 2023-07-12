422270e1bc17e3bafa6169b26362ab8eab646189
berlynncwy <123068804+berlynncwy@users.noreply.github.com>
2023-07-12 22:42:30 +0800

    Add Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,52 +1,102 @@
-import React from "react";
+import bodyParser from "body-parser";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    console.log(email);
+    event.preventDefault();
+
+    if (email == "" || password == "") {
+      setErrorMsg("** All fields must be filled.");
+    } else {
+      fetch("api/user/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      })
+        .then((res) => {
+          if (res.okay) {
+            res.json().then((msg) => "Logged in sucessfully");
+          } else {
+            res.json().then((msg) => setErrorMsg(msg.error));
+          }
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    }
+  };
   return (
-    <form action="" className="mx-auto mb-0 mt-8 max-w-md space-y-4">
+    <>
       <div>
-        <label htmlFor="username" className="sr-only">
-          Username
-        </label>
+        <p className="error-msg pt-4">{errorMsg}</p>
+      </div>
+      <form
+        onSubmit={submitHandler}
+        className="mx-auto mb-0 mt-8 max-w-md space-y-4"
+      >
+        <div>
+          <label htmlFor="email" className="sr-only">
+            Email
+          </label>
 
-        <div className="relative">
-          <input
-            type="text"
-            className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-            placeholder="Enter username"
-          />
+          <div className="relative">
+            <input
+              type="email"
+              className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+              placeholder="Enter email"
+              value={email}
+              onChange={handleEmailChange}
+            />
+          </div>
         </div>
-      </div>
 
-      <div>
-        <label htmlFor="password" className="sr-only">
-          Password
-        </label>
+        <div>
+          <label htmlFor="password" className="sr-only">
+            Password
+          </label>
 
-        <div className="relative">
-          <input
-            type="password"
-            className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-            placeholder="Enter password"
-          />
+          <div className="relative">
+            <input
+              type="password"
+              className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+              placeholder="Enter password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </div>
         </div>
-      </div>
 
-      <div className="flex items-center justify-between">
-        <p className="text-sm text-gray-500">
-          Do not have an account?
-          <a className="ml-1 underline" href="/signup">
-            Sign up here!
-          </a>
-        </p>
+        <div className="flex items-center justify-between">
+          <p className="text-sm text-gray-500">
+            Do not have an account?
+            <Link to="/signup" className="ml-1 underline">
+              Sign up here!
+            </Link>
+          </p>
 
-        <button
-          type="submit"
-          className="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
-        >
-          Sign in
-        </button>
-      </div>
-    </form>
+          <button
+            type="submit"
+            onClick={submitHandler}
+            className="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white hover:bg-blue-600"
+          >
+            Sign in
+          </button>
+        </div>
+      </form>
+    </>
   );
 }
 
