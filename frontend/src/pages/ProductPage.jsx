e656b6ee72e1e8450f9de61c43e0b8203b758d2f
berlynncwy <123068804+berlynncwy@users.noreply.github.com>
2023-07-13 14:36:51 +0800

    Create ProductPage.jsx

---
+++ frontend/src/pages/ProductPage.jsx
@@ -0,0 +1,47 @@
+import { Row, Col, Container } from "react-bootstrap";
+import { useEffect, useState } from "react";
+
+import ProductItem from "../components/ProductItem";
+
+const ProductPage = () => {
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    fetch("api/products")
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        console.log("======success=======");
+        console.log(res);
+        setProduct(res);
+      })
+      .catch((err) => {
+        console.log("======failure=======");
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>Products</h1>
+      <Container className="mb-5">
+        <Row className="gap-5 justify-start ">
+          {product.map(({ _id, name, unitDetails, price, image }) => (
+            <Col key={_id} sm={12} md={6} lg={4} xl={3} className="gap-0">
+              <ProductItem
+                _id={_id}
+                name={name}
+                unitDetails={unitDetails}
+                price={price}
+                image={image}
+              />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default ProductPage;
