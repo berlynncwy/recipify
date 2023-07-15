59b0e58ce6faab0bbf449e38d09e47ef5c1f7d85
berlynncwy <123068804+berlynncwy@users.noreply.github.com>
2023-07-15 18:45:43 +0800

    Create SingleProductPage.jsx

--- backend/routes/ProductRoutes.js
+++ backend/routes/ProductRoutes.js
@@ -9,12 +9,18 @@ const router = express.Router();
 // get all products
 router.get("/", asyncHandler(async (req, res) => {
     const products = await Product.find({});
+    if (!products) {
+        res.status(404).json("No product found");
+        return;
+    }
     res.status(200).json(products);
 }));
 
 // get a single product
 router.get("/:id", asyncHandler(async (req, res) => {
-    const id = req.params.id;
+    const { id } = req.params;
+    console.log(id);
+
     // if invalid product id format
     if (!mongoose.Types.ObjectId.isValid(id)) {
         res.status(404).json({ error: "Invalid product ID" });
---
+++ frontend/src/pages/SingleProductPage.jsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from "react";
+import { Row, Col, Button, Dropdown } from "react-bootstrap";
+
+import { useParams } from "react-router-dom";
+import QuantityButton from "../components/QuantityButton";
+
+const SingleProductPage = () => {
+  const [product, setProduct] = useState({});
+  const [productState, setProductState] = useState({});
+  const [quantity, setQuantity] = useState(0);
+
+  const { id } = useParams();
+  const url = window.location.origin + "/api/products/" + id;
+  //   console.log(window.location);
+
+  useEffect(() => {
+    fetch(url)
+      //   .then((res) => console.log(res))
+      .then((res) => res.json())
+      .then((res) => {
+        setProduct(res);
+
+        if (res.stock <= 0) {
+          productState.stockStatus = "Out of Stock";
+          productState.colour = "#bd6868";
+        } else if (res.stock < 10) {
+          productState.stockStatus = "Low in stock";
+          productState.colour = "#998353";
+        } else {
+          productState.stockStatus = "Available";
+          productState.colour = "#68bd83";
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const submitHandler = () => {
+    console.log(quantity);
+    console.log(product._id);
+
+    // pass data inito cart
+    // to do !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    useEffect(() => {
+      fetch("api/:userid/cart", { method: "POST" });
+    });
+  };
+
+  let cssDetail = productState.colour;
+
+  return (
+    <>
+      <div className=" flex justify-center">
+        <Row className="place-content-center">
+          <Col className=" flex flex-col justify-center">
+            <img src={product.image} alt="carrot" className="product-image" />
+          </Col>
+          <Col
+            as="div"
+            className="product-detail max-w-full flex flex-col justify-center"
+          >
+            <h2>{product.name}</h2>
+            {/* <div className="text-"> */}
+            <Row>
+              <Col className="">
+                <p className="product-l">Brand :</p>
+                <p className="product-l">Unit :</p>
+                <p className="product-l">Price :</p>
+                <p className="product-l">Stock status :</p>
+                <p className="product-l">Quantity :</p>
+              </Col>
+
+              <Col className="">
+                <p className="product-r">{product.brand}</p>
+                <p className="product-r">{product.unitDetails}</p>
+                <p className="product-r">${product.price}</p>
+                <p
+                  style={{ background: cssDetail }}
+                  className="product-status inline-block "
+                >
+                  {productState.stockStatus}
+                </p>
+                <QuantityButton
+                  value={quantity}
+                  setValue={setQuantity}
+                  min={0}
+                  max={10}
+                />
+                <Button
+                  type="submit"
+                  onClick={submitHandler}
+                  className="mt-2 btn-outline-secondary btn-sm"
+                  disabled={quantity === 0 || product.stock < 0}
+                >
+                  Add to Cart
+                </Button>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
+};
+
+export default SingleProductPage;
