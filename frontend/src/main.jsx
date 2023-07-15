7a6641011272828b906b523180fee6b1838c3289
berlynncwy <123068804+berlynncwy@users.noreply.github.com>
2023-07-15 21:56:57 +0800

    Update routes in main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,18 +14,22 @@ import App from "./App.jsx";
 import ErrorPage from "./pages/ErrorPage";
 import Cart from "./pages/Cart";
 import MyAccount from "./pages/MyAccount";
-import Product from "./components/Product";
 import HomePage from "./pages/HomePage";
 import RecipePage from "./pages/RecipePage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
+import ProductPage from "./pages/ProductPage";
+import SingleProductPage from "./pages/SingleProductPage";
+import SingleRecipePage from "./pages/SingleRecipePage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomePage />} />
-      <Route path="/recipe/:id/" element={<RecipePage />} />
-      <Route path="/products" element={<Product />} />
+      <Route path="/recipe/:id" element={<SingleRecipePage />} />
+      <Route path="/recipes" element={<RecipePage />} />
+      <Route path="/products" element={<ProductPage />} />
+      <Route path="/products/:id" element={<SingleProductPage />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/account" element={<MyAccount />} />
       <Route path="/login" element={<LoginPage />} />
