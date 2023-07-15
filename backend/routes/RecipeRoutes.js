144135a4c5c2cea474d8b774c65f2e82f23a215d
berlynncwy <123068804+berlynncwy@users.noreply.github.com>
2023-07-15 21:53:08 +0800

    Update RecipeRoutes

--- backend/routes/RecipeRoutes.js
+++ backend/routes/RecipeRoutes.js
@@ -1,24 +1,38 @@
 import express from "express";
+import mongoose from "mongoose";
 
 import asyncHandler from "../middleware/asyncHandler.js";
 import Recipe from "../models/RecipeModel.js";
 
 const router = express.Router();
 
+// get all recipes
 router.get("/", asyncHandler(async (req, res) => {
     const recipes = await Recipe.find({});
-    res.json(recipes);
-}));
-
-router.get("/:id", asyncHandler(async (req, res) => {
-    const recipe = await Recipe.findById(req.params.id);
-
-    if (recipe) {
-        return res.json(recipe);
+    if (!recipes) {
+        res.status(404).json("No product found");
+        return;
     }
-    res.status(404).json({ message: "Product not found" });
+    res.status(200).json(recipes);
 }));
 
+// get single recipe
+router.get("/:id", asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    console.log(id);
 
+    // if invalid recipe id format
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        res.status(404).json({ error: "Invalid recipe ID" });
+        return;
+    }
+    const recipe = await Recipe.findById(req.params.id);
+    // if no recipe found
+    if (!recipe) {
+        res.status(404).json({ message: "Product not found" });
+        return;
+    }
+    res.status(200).json(recipe);
+}));
 
 export default router;
