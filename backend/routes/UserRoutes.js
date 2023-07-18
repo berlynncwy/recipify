bfdb35b96b2b22380805fc51322950be790b370e
berlynncwy <123068804+berlynncwy@users.noreply.github.com>
2023-07-18 22:31:22 +0800

    Add cart route in UserRoutes

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -3,14 +3,15 @@ import bodyParser from 'body-parser';
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 
-const createToken = (_id) => {
-  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' });
-};
-
+import requireAuth from "../middleware/requireAuth.js";
 import asyncHandler from "../middleware/asyncHandler.js";
 import Customer from "../models/CustomerModel.js";
 import User from "../models/UserModel.js";
 import Recipe from "../models/RecipeModel.js";
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' });
+};
 
 const router = express.Router();
 
@@ -24,20 +25,6 @@ router.get("/getemail", asyncHandler(async (req, res) => {
   console.log(req.query.email);
   const user = await User.findOne({ email: req.query.email });
   res.json(user);
-}));
-
-router.get("/:id", asyncHandler(async (req, res) => {
-  console.log(req.params.id);
-  const user = await Customer.findById(req.params.id);
-}));
-
-// create new recipe route
-router.post("/recipe", bodyParser.json(), asyncHandler(async (req, res) => {
-  const json = req.body;
-  const recipe = new Recipe(json);
-  let saveRecipe = await recipe.save();
-  res.json(recipe);
-  console.log("Recipe created!");
 }));
 
 // user login
@@ -87,8 +74,51 @@ router.post("/signup", bodyParser.json(), asyncHandler(async (req, res) => {
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
-  ;
-
 }));
+
+router.get("/:id", asyncHandler(async (req, res) => {
+  console.log(req.params.id);
+  const user = await Customer.findById(req.params.id);
+}));
+
+// needs to be authenticated to create new recipe
+router.use(requireAuth);
+
+// create new recipe route
+router.post("/newrecipe", bodyParser.json(), asyncHandler(async (req, res) => {
+  const json = req.body;
+  const recipe = new Recipe(json);
+  let saveRecipe = await recipe.save();
+  res.json(recipe);
+  console.log("Recipe created!");
+}));
+
+//
+router.post("/cart", bodyParser.json(), asyncHandler(async (req, res) => {
+  console.log(req.body);
+  const json = req.body;
+  const email = json.user.email;
+  try {
+    const user = await User.findOne({ email: email });
+    if (user) {
+      const item = {
+        name: json.name,
+        quantity: json.quantity,
+        image: json.image,
+        price: json.price,
+        product: json._id
+      };
+      user.cart.push(item);
+      await user.save();
+      console.log("Product added to cart");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: error.message });
+  }
+}
+
+
+));
 
 export default router;
