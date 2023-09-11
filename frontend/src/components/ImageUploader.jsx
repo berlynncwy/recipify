6c824983e97380d089cd8648a3cc502462030782
berlynncwy <123068804+berlynncwy@users.noreply.github.com>
2023-09-11 18:15:08 +0800

    Add image size limit to ImageUploader

--- frontend/src/components/ImageUploader.jsx
+++ frontend/src/components/ImageUploader.jsx
@@ -2,12 +2,26 @@ import React, { useState } from "react";
 
 const ImageUploader = () => {
   const [image, setImage] = useState();
+  const [error, setError] = useState(null);
 
   const handleChange = (event) => {
-    console.log(event);
-    console.log(event.target.files);
-    setImage(URL.createObjectURL(event.target.files[0]));
+    const selectedFile = event.target.files[0];
+
+    if (selectedFile) {
+      const maxSize = 5 * 1024 * 1024; // 5MB in bytes
+
+      if (selectedFile.size <= maxSize) {
+        // File size is within the limit
+        setImage(URL.createObjectURL(selectedFile));
+        setError(null);
+      } else {
+        // File size exceeds the limit
+        setError("Image size exceeds 5MB. Please choose a smaller image.");
+        setImage(null);
+      }
+    }
   };
+
   return (
     <>
       <div>
@@ -18,7 +32,8 @@ const ImageUploader = () => {
           onChange={handleChange}
           className="mb-4"
         ></input>
-        <img src={image}></img>
+        {error && <p className="text-danger">{error}</p>}
+        {image && <img src={image} alt="Uploaded" />}
       </div>
     </>
   );
