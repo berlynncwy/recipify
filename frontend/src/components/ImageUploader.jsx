5281706192b685d4e1ab2560804d73629e46f1c6
berlynncwy <123068804+berlynncwy@users.noreply.github.com>
2023-09-12 13:58:42 +0800

    Update ImageUploader - convert image to Base64

--- frontend/src/components/ImageUploader.jsx
+++ frontend/src/components/ImageUploader.jsx
@@ -11,13 +11,24 @@ const ImageUploader = () => {
       const maxSize = 5 * 1024 * 1024; // 5MB in bytes
 
       if (selectedFile.size <= maxSize) {
-        // File size is within the limit
-        setImage(URL.createObjectURL(selectedFile));
-        setError(null);
+        const reader = new FileReader();
+        reader.readAsDataURL(selectedFile);
+
+        reader.onload = () => {
+          console.log(reader.result);
+          setImage(reader.result);
+          setError(null);
+        };
+
+        reader.onerror = (error) => {
+          console.log("Error: ", error);
+          setError("Error reading the file.");
+          setImage(null);
+        };
       } else {
-        // File size exceeds the limit
         setError("Image size exceeds 5MB. Please choose a smaller image.");
         setImage(null);
+        event.target.value = null; // Clear the file input
       }
     }
   };
@@ -28,7 +39,7 @@ const ImageUploader = () => {
         <label>Upload Image</label>
         <input
           type="file"
-          accept="image/*"
+          accept=".jpeg, .png, .jpg"
           onChange={handleChange}
           className="mb-4"
         ></input>
