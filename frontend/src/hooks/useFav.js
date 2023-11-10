d65ebbe7ff16a8a993ad6388006d2851642fb181
berlynncwy <123068804+berlynncwy@users.noreply.github.com>
2023-11-11 02:10:51 +0800

    Add useFav hook

---
+++ frontend/src/hooks/useFav.js
@@ -0,0 +1,77 @@
+import { AuthContext } from "../context/AuthContext.jsx";
+import { createContext, useCallback, useContext, useEffect, useMemo, useState } from "react";
+import { useAuthContext } from "./useAuthContext.js";
+
+export const useFav = () => {
+    const { user } = useAuthContext();
+
+    const [favourites, setFav] = useState([]);
+
+    useEffect(() => {
+        if (user == null) return;
+
+        const requestOptions = {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${user.token}`,
+            },
+        };
+        fetch("api/recipes/favourites/ids", requestOptions)
+            .then((res) => {
+                return res.json();
+            })
+            .then((json) => {
+                if (json.favourites != null) {
+                    console.log(json);
+                    setFav(json.favourites);
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, [user]);
+
+
+    const onFavourite = useCallback((id) => {
+        if (user == null) return;
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${user.token}`,
+            },
+            body: JSON.stringify({
+                recipeID: id,
+            }),
+        };
+        fetch("/api/recipes/favourites", requestOptions)
+            .then((res) => {
+                return res.json();
+            })
+            .then((json) => {
+                if (json.favourites != null) {
+                    setFav(json.favourites);
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, [user]);
+
+
+    const favSet = useMemo(() => {
+        const set = new Set();
+        for (const id of favourites) {
+            set.add(id);
+        }
+        return set;
+    }, [favourites]);
+
+    const isFav = useCallback((id) => {
+        const isFavourite = user ? favSet.has(id) : undefined;
+        return isFavourite;
+    }, [user, favSet]);
+
+
+    return { fav: favourites, isFav, favSet, onFavToggle: onFavourite };
+};
