d741fad26d94fbeb466d7ade80e8586578297ba3
berlynncwy <123068804+berlynncwy@users.noreply.github.com>
2023-11-14 14:49:10 +0800

    Update homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,9 +2,11 @@ import { Row, Col } from "react-bootstrap";
 import { useEffect, useState } from "react";
 
 import RecipeItem from "../components/RecipeItem";
+import { useFav } from "../hooks/useFav";
 
 const HomePage = () => {
   const [recipe, setRecipe] = useState([]);
+  const { isFav, onFavToggle } = useFav();
   useEffect(() => {
     fetch("api/recipes")
       .then((resp) => {
@@ -26,18 +28,24 @@ const HomePage = () => {
       <h1>Popular Recipes</h1>
       <Row>
         {recipe.map(
-          ({ title, _id, description, rating, numReviews, image }) => (
-            <Col key={_id} sm={12} md={6} lg={4} xl={3}>
-              <RecipeItem
-                title={title}
-                id={_id}
-                description={description}
-                rating={rating}
-                noOfReviews={numReviews}
-                image={image}
-              />
-            </Col>
-          )
+          ({ _id, title, description, rating, numReviews, image }) => {
+            const favourite = isFav(_id);
+
+            return (
+              <Col key={_id} sm={12} md={6} lg={4} xl={3} className="gap-0">
+                <RecipeItem
+                  title={title}
+                  _id={_id}
+                  description={description}
+                  rating={rating}
+                  noOfReviews={numReviews}
+                  image={image}
+                  favourite={favourite}
+                  onFavourite={() => onFavToggle(_id)}
+                />
+              </Col>
+            );
+          }
         )}
       </Row>
     </>
